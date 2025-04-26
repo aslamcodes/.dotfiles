76dbd7fd2a2fd576c1978f8465c95616c2cb44ea
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-04-26 18:10:38 +0530

    Config update

---
+++ nvim/lua/plugins/aw.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "lowitea/aw-watcher.nvim",
+        opts = {
+            aw_server = {
+                host = "127.0.0.1",
+                port = 5600,
+            },
+        },
+    }
+}
